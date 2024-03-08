d14d08de915edec00e83557bb644a13603842a2f
rasulali <resul.eliyev2000@mail.ru>
2024-03-09 01:01:11 +0400

    test vercel build

---
+++ README.md
@@ -0,0 +1,1 @@
+# Development page of Alioglu
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  // only for github action
-  // basePath: process.env.NODE_ENV === "production" ? "/alioglu" : undefined,
-  // assetPrefix: process.env.NODE_ENV === "production" ? "/alioglu/" : undefined,
-  // imagesPublicPath: process.env.NODE_ENV === "production" ? "/alioglu/_next/static/images/" : undefined,
-
-  // for dev server
-  // images: {
-  //   unoptimized: true,
-  // },
 };
 
 export default nextConfig;
