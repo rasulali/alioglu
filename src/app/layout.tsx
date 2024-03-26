7f51363795ea7f9647d2a5fa2e3cbb8992ae511f
rasulali <resul.eliyev2000@mail.ru>
2024-03-26 18:48:13 +0400

    edge functions and nodejs compatibility flag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   description: "Xəyalları Dizayn, Gələcəyi İnşa Edirik!",
 };
 
+export const runtime = 'edge'
+
 export default function RootLayout({
   children,
 }: {
