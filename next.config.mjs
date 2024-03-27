b5e341b45d262d482d1f829a8c13e54902c3cc89
rasulali <resul.eliyev2000@mail.ru>
2024-03-27 22:36:16 +0400

    removed cf pages support attempt 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
-
-// note: the if statement is present because you
-//       only need to use the function during development
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform({
-    persist: true
-  })
-}
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  //images: {
-  //  loader: 'custom',
-  //  loaderFile: './imageLoader.js',
-  //},
-}
+const nextConfig = {}
 
 export default nextConfig
