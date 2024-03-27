a436cf2ec6ac220b1aec7d140c7e40366ad48774
rasulali <resul.eliyev2000@mail.ru>
2024-03-27 06:22:57 +0400

    nextjs image optimization v4

--- imageLoader.js
+++ imageLoader.js
@@ -1,5 +1,4 @@
-// Docs: https://developers.cloudflare.com/images/url-format
 export default function cloudflareLoader({ src, width, quality }) {
   const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
-  return `https://alioglu.pages.dev/cdn-cgi/image/${params.join(',')}/${src}`
+  return `/cdn-cgi/image/${params.join(',')}/${src}`
 }
